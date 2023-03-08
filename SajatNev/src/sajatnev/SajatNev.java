49e9f52138b687fac03dce3b8c523fbb85389157
Péter Csolti <csolti@szamalk-szalezi.hu>
2023-03-08 14:57:32 +0100

    tombben 5db veletlenszam

--- SajatNev/src/sajatnev/SajatNev.java
+++ SajatNev/src/sajatnev/SajatNev.java
@@ -1,9 +1,16 @@
 package sajatnev;
 
+import java.util.Random;
+
 public class SajatNev {
 
+    private static Random rnd = new Random();
+    
     public static void main(String[] args) {
-        int[] tomb = {4, 2};
+        int[] tomb = new int[5];
+        for (int i = 0; i < 5; i++) {
+            tomb[i] = rnd.nextInt();
+        }
     }
     
 }
