f00419b442e4feeff7c8d2cba9b07270177672cb
Péter Csolti <csolti@szamalk-szalezi.hu>
2023-03-08 14:58:56 +0100

    veletlenszamok [-5; 15]

--- SajatNev/src/sajatnev/SajatNev.java
+++ SajatNev/src/sajatnev/SajatNev.java
@@ -9,7 +9,9 @@ public class SajatNev {
     public static void main(String[] args) {
         int[] tomb = new int[5];
         for (int i = 0; i < 5; i++) {
-            tomb[i] = rnd.nextInt();
+            //tomb[i] = rnd.nextInt(-5, 16);
+            /*                   (felso - also + 1) + also */
+            tomb[i] = rnd.nextInt(15 - -5 + 1 ) + -5;
         }
     }
     
