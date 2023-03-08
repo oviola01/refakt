0d69e633c36dd7d72ff59f2e7238e1d580c59f33
Péter Csolti <csolti@szamalk-szalezi.hu>
2023-03-08 15:01:06 +0100

    int[] feltoltes(int)

--- SajatNev/src/sajatnev/SajatNev.java
+++ SajatNev/src/sajatnev/SajatNev.java
@@ -7,16 +7,18 @@ public class SajatNev {
     private static Random rnd = new Random();
     
     public static void main(String[] args) {
-        feltolt(5);
+        int[] tomb = feltolt(5);
     }
 
-    private static void feltolt(int db) {
+    private static int[] feltolt(int db) {
         int[] tomb = new int[db];
         for (int i = 0; i < db; i++) {
             //tomb[i] = rnd.nextInt(-5, 16);
             /*                   (felso - also + 1) + also */
             tomb[i] = rnd.nextInt(15 - -5 + 1 ) + -5;
         }
+        
+        return tomb;
     }
     
 }
